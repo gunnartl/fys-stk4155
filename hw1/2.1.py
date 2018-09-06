017542d4f2aa9e315004b77f9f3652abb6bd7959
Gunnar <gunnartl@uio.no>
2018-09-06 23:21:36 +0200

    jajamensan

--- hw1/2.1.py
+++ hw1/2.1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 def reg2(x,y):
     X = np.c_[np.ones(n),x,x**2]
@@ -10,15 +11,26 @@ def reg2(x,y):
     return xnew, ynew
 
 
+#oppg 1
 n= 100
+x = np.random.rand(n)
+y = 5*x*x+0.1*np.random.randn(n)
 
-plipp = np.random.rand(n)
-plopp = 5*plipp*plipp+0.1*np.random.randn(n)
-
-x,y = reg2(plipp,plopp)
+x_plot,y_plot = reg2(x,y)
 
 
 import matplotlib.pyplot as plt
-plt.scatter(plipp,plopp)
-plt.plot(x,y,'r')
+plt.scatter(x,y)
+plt.plot(x_plot,y_plot,'r')
+#plt.show()
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+X= x[:,np.newaxis]
+X_plot= x_plot[:,np.newaxis]
+model = make_pipeline(PolynomialFeatures(2), Ridge())
+model.fit(X,y)
+y_plot2 = model.predict(X_plot)
+plt.plot(x_plot,y_plot2)
 plt.show()
