8d7fbdc24ac1da3071b186340104b505e5b42dd0
Gunnar Thorsen Liahjell <gunnar@Abel-2.0>
2018-09-25 16:11:52 +0200

    MASETOBIAS

--- project1/testfil.py
+++ project1/testfil.py
@@ -26,6 +26,17 @@ def OLS(x,y,z,n,deg):
     plutt = znew.reshape((n,n))
     return xnew, ynew, plutt
 
+def ridge(x,y,z,n,deg,lambd):
+    X = polynomial_this(x,y,deg)
+    beta = np.linalg.inv(X.T.dot(X)+lambd*np.identity(21)).dot(X.T.dot(z))
+    
+    xnew = np.linspace(0,1,n)
+    ynew = np.linspace(0,1,n)
+    xnew , ynew = np.meshgrid(xnew,ynew)
+    Xnew = polynomial_this(xnew.reshape(n**2,1),ynew.reshape(n**2,1),deg)
+    znew = Xnew.dot(beta)
+    plutt = znew.reshape((n,n))
+    return xnew, ynew, plutt
 def FRANK(x,y):
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
@@ -34,7 +45,7 @@ def FRANK(x,y):
     return term1+term2+term3+term4
 
 #oppg 1
-n = 200
+n = 500
 deg = 5
 x = (np.random.rand(n))
 y = (np.random.rand(n))
@@ -45,7 +56,7 @@ x1d = x.reshape((n**2,1))
 y1d = y.reshape((n**2,1))
 
 
-z = FRANK(x1d,y1d) #+0.5*np.random.randn(n*n,1)
+z = FRANK(x1d,y1d) +5*np.random.randn(n*n,1)
 
 x_plot,y_plot , z_plot = OLS(x1d,y1d,z,n,deg)
 temp = z_plot.reshape((n**2,1))
@@ -73,7 +84,7 @@ ax = fig.gca(projection="3d")
 surf = ax.plot_surface(x_plot,y_plot,z_plot, cmap=cm.coolwarm,
 linewidth=0, antialiased=False)
 # Customize the z axis.
-ax.set_zlim(-0.10, 1.40)
+#ax.set_zlim(-0.10, 1.40)
 ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
 # Add a color bar which maps values to colors.
