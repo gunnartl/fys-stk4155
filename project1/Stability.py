07199b4aa40cc9bdb2c2db1b3877b337c649e519
Gunnar <gunnartl@uio.no>
2018-10-04 23:09:20 +0200

    nesten ferdig

--- project1/Stability.py
+++ project1/Stability.py
@@ -1,16 +1,46 @@
 import reg_class
+from reg_class import*
 
+MSE_vec = np.zeros((3,20,200))
+R2_vec  = np.zeros_like(MSE)
 
-for n in range(10,100,10):
-    for i in range(10):
+for i in range(20):
+    print(i)
+    for j in range(200):
+        n = (i+11)
+        x = np.sort((np.random.rand(n)))
+        y = np.sort((np.random.rand(n)))
         
-    x = np.sort((np.random.rand(n)))
-    y = np.sort((np.random.rand(n)))
-    
-    x,y = np.meshgrid(x,y)
-    
-    deg = 5
-    z = reg_class.FRANK(x.ravel(),y.ravel()) #+0.5*np.random.randn(n*n,1)
-    X = polynomial_this(x,y,deg)
-    print(X.shape)
-    FRANK = regression(X,z)
+        x,y = np.meshgrid(x,y)
+        
+        deg = 5
+        z = reg_class.FRANK(x.ravel(),y.ravel()) +0.3*np.random.randn(n*n,)
+        X = polynomial_this(x,y,deg)
+        from sklearn.model_selection import train_test_split
+        X,X_test,z,z_test = train_test_split(X, z, test_size = 0.2)
+        
+        FRANK = regression(X,z)
+        OLS_beta = FRANK.OLS()
+        MSE_vec[0,i,j] = MSE(z_test,X_test.dot(OLS_beta))
+
+        ridge_beta = FRANK.ridge(0.01)
+        MSE_vec[1,i,j] = MSE(z_test,X_test.dot(ridge_beta))
+
+
+        lasso_beta = FRANK.lasso(0.01)
+        MSE_vec[2,i,j] = MSE(z_test,X_test.dot(lasso_beta))
+        
+
+
+MSE_mean = np.mean(MSE_vec,axis=2)
+var = np.var(MSE_vec, axis=2)
+n = np.array(range(10))*10
+import matplotlib.pyplot as plt
+plt.plot(MSE_mean[0])
+plt.plot(MSE_mean[1])
+plt.plot(MSE_mean[2])
+plt.legend(["OLS","RIDGE","lasso"])
+#plt.semilogy()
+plt.show()
+
+        
