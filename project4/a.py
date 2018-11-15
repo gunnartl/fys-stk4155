16ca9bb43148c113334e51f0f128956daad2eb70
Gunnar <gunnartl@uio.no>
2018-11-15 18:07:47 +0100

    Ferri for dagen

---
+++ project4/a.py
@@ -0,0 +1,85 @@
+import numpy as np
+from numba import njit
+import time
+
+
+def init_state(L):    
+    state0 = np.random.choice((-1,1),L*L).reshape((L,L))
+    E0 = 0
+    for i in range(L):
+        for j in range(L):
+            E0 -= state0[i,j]*state0[i,j-1]+state0[i,j]*state0[i-1,j]
+    M0 = np.sum(state0)
+    return state0,E0,M0
+
+@njit
+def mcmc(states,E,M,cycles,temp):
+    Eav  = E
+    Mav  = M
+    E2av = E**2
+    M2av = M**2
+    Mabs = np.abs(M)
+    L = states.shape[0]
+    for i in range(cycles):
+        #flip random spin
+        m = np.random.randint(L)
+        n = np.random.randint(L)
+
+        #compute delta
+        left = states[m-1,n]
+        right = states[(m+1)%L,n]
+        up = states[m,n-1]
+        down = states[m,(n+1)%L]
+        
+
+        delta = 2*states[m,n] * (left+right+up+down)
+        
+        #check
+        if np.random.random() <= np.exp(-delta/temp):
+            states[m,n] *= -1
+            E      += delta
+            M      += 2*states[m,n]
+            #E2     += E**2
+            #M2     += M**2
+        Eav += E
+        Mav += M
+        M2av += M**2
+        E2av += E**2
+        Mabs += np.abs(M)
+            
+            
+    Eav  /= cycles
+    E2av /= cycles
+    M2av /= cycles
+    Mav  /= cycles
+    Mabs /= cycles
+    
+    heatcap = (E2av-Eav**2)/(temp**2)
+    sucept  = (M2av-Mav**2)/temp
+    return Eav/L**2,heatcap/L**2,Mabs/L**2,sucept/L**2
+
+if __name__ == "__main__":
+    L = 2
+
+    #state1,E1,M1 = init_state(L)
+    temp = 1 # temperature
+    start = time.time()
+    #cycles=int(1e7)
+    cycles = 10**np.array([2,3,4,5])
+    energy = np.zeros(len(cycles))
+    magnet = np.zeros(len(cycles))
+    Cv     = np.zeros(len(cycles))
+    sucept = np.zeros(len(cycles))
+    
+    #Checks energy, heatcapacity, megnetisation and suceptebility for several 
+    #numbers of cycles
+    for i,j in enumerate(cycles):
+        state0,E0,M0 = init_state(L)
+        energy[i], Cv[i], magnet[i],sucept[i]= mcmc(state0,E0,M0,j,temp)
+
+
+    
+    stop = time.time()
+    
+    np.savetxt("tabell_a.txt",np.c_[cycles,energy,magnet,Cv,sucept]) # saves a NEAT table
+    print("time",stop-start)
--- project4/test.py
+++ project4/test.py
@@ -9,58 +9,78 @@ def init_state(L):
     for i in range(L):
         for j in range(L):
             E0 -= state0[i,j]*state0[i,j-1]+state0[i,j]*state0[i-1,j]
-    M0 = sum(state0)
-    return state0,E0
+    M0 = np.sum(state0)
+    return state0,E0,M0
 
 @njit
-def mcmc(states,E0,cycles,temp):
+def mcmc(states,E0,M0,cycles,temp):
     E = np.zeros(cycles)
-    #M = np.zeros(cycles)
-    E[0] = E0
+    M = np.zeros(cycles)
     accept = np.zeros(cycles)
-    #M[0] = M0
+    E[0] = E0
+    M[0] = M0
     L = states.shape[0]
     for i in range(1,cycles):
         #flip random spin
-        x = np.random.randint(L)
-        y = np.random.randint(L)
+        m = np.random.randint(L)
+        n = np.random.randint(L)
         #print(states)
         #print('')
         
         #energy_delta
-        left = states[x-1,y]
-        right = states[(x+1)%L,y]
-        up = states[x,y-1]
-        down = states[x,(y+1)%L]
+        left = states[m-1,n]
+        right = states[(m+1)%L,n]
+        up = states[m,n-1]
+        down = states[m,(n+1)%L]
         
 
-        delta = 2*states[x,y] * (left+right+up+down)
+        delta = 2*states[m,n] * (left+right+up+down)
         
         #check
         if np.random.random() <= np.exp(-delta/temp):
             #print("pip")
-            states[x,y] *= -1
+            states[m,n] *= -1
             E[i] = E[i-1]+delta
+            M[i] = M[i-1]+2*states[m,n]
             accept[i] = 1.
             #E2[i] = E[i]**2
         else:
             E[i] = E[i-1]
-        #print(states)
+            M[i] = M[i-1]
         #var_E = (E[i]**2 - E2[i])
-    return E, states, accept
+    return E, M, states, accept
 
 if __name__ == "__main__":
-    L = 20
-    state0,E0 = init_state(L)
-    temp = 2.4
+    L = 40
+    rand_state0,E0,M0 = init_state(L)
+    rand_state1,E1,M1 = init_state(L)
+    
+    ordered_state0 = np.ones((L,L))
+    ordered_state0 = np.ones((L,L))
+    temp = 1
     start = time.time()
-    cycles = int(2e4)
-    energy, states, accepted= mcmc(state0,E0,cycles,temp)
+    cycles = int(2e6)
+    #energies = 
+    #magnetisations
+    
+    #for i in cycles = 
+    energy1, magnet1,states1, accepted1= mcmc(rand_state0,E0,M0,cycles,temp)
+    energy2, magnet2,states2, accepted2= mcmc(rand_state1,E1,M1,cycles,2.4)
     
     stop = time.time()
     print(stop-start)
-    
+    #%%
     import matplotlib.pyplot as plt
-    plt.plot(np.arange(cycles),energy)
-    plt.plot(np.arange(cycles),np.cumsum(accepted))
+    plt.plot(np.arange(cycles)[::1000]/400,energy1[::1000])
+    plt.plot(np.arange(cycles)[::1000]/400,energy2[::1000])
+    plt.legend(["T=1","T=2.4"])
+    plt.xlabel("MC-cycles")
+    plt.ylabel("# of accepted states")
+    plt.grid()
     plt.show
+    #fig,ax1 = plt.subplots()
+    #ax1.plot(np.arange(cycles),energy1)
+    #ax2 = ax1.twinx()
+    #ax2.plot(np.arange(cycles),np.cumsum(accepted1))
+    #plt.imshow(states1)
+    #plt.show()
